8d40da365a6d6553aef603c326e16f41683c9588
EvgHrn <evg.hrn@gmail.com>
2018-09-07 15:01:43 +0400

    Work with db in App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createBottomTabNavigator  } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
-import ScannerScreen from './screens/ScannerScreen'
-import DetailedHistoryScreen from './screens/DetailedHistoryScreen'
-import HomeScreen from './screens/HomeScreen'
-import AddEntryScreen from './screens/AddEntryScreen'
-import db from './components/db'
+import { SQLite } from 'expo';
+import ScannerScreen from './screens/ScannerScreen';
+import DetailedHistoryScreen from './screens/DetailedHistoryScreen';
+import HomeScreen from './screens/HomeScreen';
+import AddEntryScreen from './screens/AddEntryScreen';
+
+const db = SQLite.openDatabase('db.db');
 
 const RootStack = createBottomTabNavigator(
   {
@@ -38,7 +40,34 @@ const RootStack = createBottomTabNavigator(
 export default class App extends React.Component {
 
   componentDidMount() {
-    db.createTable();
+    database.transaction(tx => {
+      tx.executeSql(
+
+      /*
+
+      id
+      equipTitle
+      equipId
+      dateTime
+      entry
+
+      */
+
+      'create table if not exists entries (id integer primary key not null, equipTitle text, equipId text, dateTime text, entry text);'
+      );
+    });
+  }
+
+  addEntry(equipTitle, equipId, dateTime, entry) {
+
+    db.transaction(tx => {
+            tx.executeSql('insert into entries (equipTitle, equipId, dateTime, entry) values (?, ?, ?, ?)', [equipTitle, equipId, dateTime, entry]);
+            tx.executeSql('select * from entries', [], (_, { rows }) =>
+            console.log(JSON.stringify(rows)));
+        },
+        null,
+        this.update
+    );
   }
 
   render() {
