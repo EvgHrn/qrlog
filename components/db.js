3c0d7bfbcdeff74ec3d09e184f7029d3ac37ba85
EvgHrn <evg.hrn@gmail.com>
2018-09-11 17:25:00 +0400

    Add 'not null' to tables when nessesary

--- components/db.js
+++ components/db.js
@@ -7,7 +7,7 @@ export const createTablesIfNotExist = () => {
         tx.executeSql(
            'create table if not exists equipment ( \
                 id integer primary key not null, \
-                equipTitle text, \
+                equipTitle text not null, \
                 equipId text unique \
             );'
         );
@@ -18,10 +18,10 @@ export const createTablesIfNotExist = () => {
             'PRAGMA foreign_keys=on; \
             create table if not exists entries ( \
                 id integer primary key not null, \
-                equipTitle text, \
+                equipTitle text not null, \
                 equipId text, \
-                dateTime text, \
-                entry text  \
+                dateTime text not null, \
+                entry text not null \
                 FOREIGN KEY (equipTitle) REFERENCES equipment(equipTitle) \
                 FOREIGN KEY (equipId) REFERENCES equipment(equipId) \
             );'
