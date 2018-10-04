1184d8b1f46cd80d0f534998399322fa8086c484
EvgHrn <evg.hrn@gmail.com>
2018-10-04 11:26:45 +0400

    Add env variables

--- components/db.js
+++ components/db.js
@@ -1,6 +1,7 @@
 import PouchDB from 'pouchdb-react-native';
 import { SQLite } from 'expo';
 import SQLiteAdapterFactory from 'pouchdb-adapter-react-native-sqlite';
+import { SERVER_IP, DB_PASS } from 'react-native-dotenv';
 
 const SQLiteAdapter = new SQLiteAdapterFactory(SQLite);
 
